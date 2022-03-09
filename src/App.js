60ed4c9e6aa78f54a056babfea4a75f75e0dfaec
bhupendar007k <bhupendar@WIL-Admins-Mac-mini.local>
2022-03-09 15:58:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Squares from "./components/squares";
 import { MainContainer, Row, Board } from "./style/boardstyle";
 
 function App() {
+  const [text, setWinner] = useState("");
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", "", ""]);
-  const [palyer, setPlayer] = useState(false);
+  const [player, setPlayer] = useState("0");
 
-  const onUserClicked = (index) => {
+  const onSquareClick = (index) => {
     let strings = Array.from(board);
-    if (strings[index]) return;
-    strings[index] = palyer ? "X" : "0";
-    setPlayer(!palyer);
+    if (strings[index]) {
+      return;
+    }
+    // conditon for each player's turn.
+    strings[index] = player ? "X" : "0";
+    setPlayer(!player);
     setBoard(strings);
   };
 
@@ -18,12 +22,15 @@ function App() {
     setBoard(["", "", "", "", "", "", "", "", "", ""]);
   };
   useEffect(() => {
-    let winner = checkWinner();
+    const winner = checkWinner();
     if (winner) {
       clearGame();
-      alert(`${winner} won the Game !`);
+      // alert(`${winner} won the Game !`);
+      const win = () => {
+        setWinner(winner);
+      };
     }
-  }, [board]);
+  });
 
   const checkWinner = () => {
     const lines = [
@@ -36,7 +43,7 @@ function App() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    console.log(board[0], board[1], board[2]);
+    // console.log(board[0], board[1], board[2]);
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
@@ -48,21 +55,22 @@ function App() {
 
   return (
     <MainContainer>
+      <p></p>
       <Board>
         <Row>
-          <Squares onClick={() => onUserClicked(0)} state={board[0]} />
-          <Squares onClick={() => onUserClicked(1)} state={board[1]} />
-          <Squares onClick={() => onUserClicked(2)} state={board[2]} />
+          <Squares onClick={() => onSquareClick(0)} state={board[0]} />
+          <Squares onClick={() => onSquareClick(1)} state={board[1]} />
+          <Squares onClick={() => onSquareClick(2)} state={board[2]} />
         </Row>
         <Row>
-          <Squares onClick={() => onUserClicked(3)} state={board[3]} />
-          <Squares onClick={() => onUserClicked(4)} state={board[4]} />
-          <Squares onClick={() => onUserClicked(5)} state={board[5]} />
+          <Squares onClick={() => onSquareClick(3)} state={board[3]} />
+          <Squares onClick={() => onSquareClick(4)} state={board[4]} />
+          <Squares onClick={() => onSquareClick(5)} state={board[5]} />
         </Row>
         <Row>
-          <Squares onClick={() => onUserClicked(6)} state={board[6]} />
-          <Squares onClick={() => onUserClicked(7)} state={board[7]} />
-          <Squares onClick={() => onUserClicked(8)} state={board[8]} />
+          <Squares onClick={() => onSquareClick(6)} state={board[6]} />
+          <Squares onClick={() => onSquareClick(7)} state={board[7]} />
+          <Squares onClick={() => onSquareClick(8)} state={board[8]} />
         </Row>
         <button className="btn" onClick={clearGame}>
           Clear Game
